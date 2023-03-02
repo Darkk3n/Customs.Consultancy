e0562bbd2472759321a9404c7594ffb61f3176ef
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-03-01 22:06:37 -0600

    Added container and content placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import { NavBar } from "./shared/components/NavBar"
 
 const App = () => {
   return (
-    <NavBar />
+    <div className="container mt-3  ml-10 w-full flex flex-col justify-center items-center">
+      <NavBar />
+      <main className="bg-white w-full h-screen text-center text-slate-900">
+        Contenido
+      </main>
+    </div>
   )
 }
 
