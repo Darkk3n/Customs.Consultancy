d9d4e2acf3a1cd420b42d1c2a8a54ef267091e0c
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-03-01 20:01:34 -0600

    Added NavBar component

---
+++ src/shared/components/NavBar.tsx
@@ -0,0 +1,27 @@
+
+export const NavBar = () => {
+   return (
+      <nav className="border-gray-200 px-2 rounded">
+         <div className="flex flex-wrap items-center justify-center mx-auto">
+            <img className='h-30 w-40 rounded-full' src='/logo.png' />
+            <ul className="flex flex-col p-4 mt-4 ml-2 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+               <li>
+                  <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Inicio</a>
+               </li>
+               <li>
+                  <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">¿Quienes Somos?</a>
+               </li>
+               <li>
+                  <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sesiones</a>
+               </li>
+               <li>
+                  <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Servicios</a>
+               </li>
+               <li>
+                  <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contacto</a>
+               </li>
+            </ul>
+         </div>
+      </nav>
+   )
+}
